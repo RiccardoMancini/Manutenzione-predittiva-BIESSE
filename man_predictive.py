73b274a3b07d083d822f80936323e2bcb013c250
unknown <riccardomancini98@gmail.com>
2023-04-04 16:48:53 +0200

    initialize repository

---
+++ man_predictive.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class PredManClass:
+    def __init__(self):
+        self.vib_foot = pd.read_excel('VibrationFootprints.xlsx', sheet_name='Foglio1')
+
+    def some_stats(self):
+        v = self.vib_foot['classe'].value_counts()
+        print('N° sample x classe:')
+        print(v)
+
+
+
+
+if __name__ == "__main__":
+    predManObj = PredManClass()
+    predManObj.some_stats()
