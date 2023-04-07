ef53d6603ea83df012b4ff9ec490570b6da70a81
Riccardo Mancini <riccardomancini98@gmail.com>
2023-04-07 16:15:23 +0200

    Update man_predictive.py

--- man_predictive.py
+++ man_predictive.py
@@ -1,17 +1,25 @@
+import numpy as np
 import pandas as pd
 import openpyxl
+import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from datetime import datetime
+from sklearn.utils import resample
+from scipy.stats import weibull_min
+import datetime as dt
+
 
 class PredManClass:
+
     def __init__(self):
         self.vib_foot = pd.read_excel('VibrationFootprints.xlsx', sheet_name='Foglio1')
 
         # conversione in timestamp
-        self.vib_foot['startDate'] = self.vib_foot['startDate'].astype('int64') // 10**9
+        self.vib_foot['startDate'] = self.vib_foot['startDate'].astype('int64') // 10 ** 9
         self.vib_foot['endDate'] = self.vib_foot['endDate'].astype('int64') // 10 ** 9
+
+        print(self.vib_foot.shape)
 
     def some_stats(self):
         v = self.vib_foot['classe'].value_counts()
@@ -22,12 +30,26 @@ class PredManClass:
         # rimuoviamo temporaneamente il campione della classe con un solo campione
         single_sample_class = None
         for c in self.vib_foot['classe'].unique():
-            print(c)
-            if (self.vib_foot[self.vib_foot['classe'] == c]).sum() == 1:
+            # print(c)
+            if self.vib_foot[self.vib_foot['classe'] == c].shape[0] == 1:
                 single_sample_class = self.vib_foot.loc[self.vib_foot['classe'] == c]
+                # print(self.vib_foot.loc[self.vib_foot['classe'] == c])
                 self.vib_foot = self.vib_foot.loc[self.vib_foot['classe'] != c]
-                break
 
+        # print(self.vib_foot.shape[0])
+
+        # Oversampling delle classi 3 e 4 e undersampling della classe 5
+        df_2 = resample(self.vib_foot[self.vib_foot['classe'] == 3], replace=True, n_samples=19, random_state=42)
+        df_1 = resample(self.vib_foot[self.vib_foot['classe'] == 4], replace=True, n_samples=19, random_state=42)
+        df_3 = self.vib_foot[self.vib_foot['classe'] == 5].sample(19)
+
+
+        # Combinare i dataframes oversampled con il dataframe originale
+        df_balanced = pd.concat([df_3, df_2, df_1])
+
+        # Verificare che le classi siano bilanciate
+        print(df_balanced['classe'].value_counts())
+        self.vib_foot = df_balanced.drop(['snMacchina', 'snEm'], axis=1)
         # Split the data into training and testing set        
         X_train, X_test, y_train, y_test = train_test_split(self.vib_foot.drop(['classe'], axis=1),
                                                             self.vib_foot['classe'],
@@ -45,18 +67,45 @@ class PredManClass:
         # Measuring accuracy on Testing Data
         print(metrics.classification_report(y_test, prediction))
         print(metrics.confusion_matrix(y_test, prediction))
+        '''metrics.ConfusionMatrixDisplay(metrics.confusion_matrix(y_test, prediction)).plot()
+        plt.show()'''
+
 
         # Plotting the feature importance for Top 10 most important columns
-        feature_importances = pd.Series(dTree.feature_importances_, index='Predictors')
-        feature_importances.nlargest(10).plot(kind='barh')
+        feature_importances = pd.Series(dTree.feature_importances_, index=X_train.columns)
+        feature_importances.nlargest(10).plot(kind='barh', fontsize=6)
+        plt.show()
 
-        # Printing some sample values of prediction
-        '''TestingDataResults = pd.DataFrame(data=X_test, columns=Predictors)
-        TestingDataResults &  # 91;'TargetColumn']=y_test
-        TestingDataResults &  # 91;'Prediction']=prediction
-        TestingDataResults.head()'''
+    def weibullDist(self):
+        df = self.vib_foot
+
+        # Calcola i parametri della distribuzione di Weibull
+        shape, loc, scale = weibull_min.fit(df['deltaDateHour'], floc=0)
+
+        # Stima il tempo di vita rimanente per ogni componente
+        df['remainingLifeTime'] = weibull_min.ppf(0.9, shape, loc=loc, scale=scale) - df['deltaDateHour']
+
+        # Converti il tempo di vita rimanente in giorni
+        df['remainingLifeTime'] = df['remainingLifeTime'] / 24
+
+        # Stampa il risultato
+        print(df[['snMacchina', 'remainingLifeTime']])
+
+
+        # Calcola la distribuzione di Weibull con i parametri shape, loc e scale
+        x = np.linspace(0, 10000, 10000)
+        pdf = weibull_min.pdf(x, shape, loc=loc, scale=scale)
+
+        # Grafica la distribuzione di Weibull
+        plt.plot(x, pdf)
+        plt.xlabel('Tempo di vita (ore)')
+        plt.ylabel('Densità di probabilità')
+        plt.title('Distribuzione di Weibull')
+        plt.show()
+
 
 if __name__ == "__main__":
     predManObj = PredManClass()
     # predManObj.some_stats()
-    predManObj.decisionTree_classifier()
+    # predManObj.decisionTree_classifier()
+    predManObj.weibullDist()
